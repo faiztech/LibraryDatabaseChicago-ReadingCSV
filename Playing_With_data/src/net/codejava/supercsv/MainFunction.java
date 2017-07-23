48df496de4d53625b01f4749dd68d91697638894
mohammedfaizuddin <mfaizudd@neiu.edu>
2017-07-23 18:25:02 -0500

    Put in User Interface

--- Playing_With_data/src/net/codejava/supercsv/MainFunction.java
+++ Playing_With_data/src/net/codejava/supercsv/MainFunction.java
@@ -1,25 +1,38 @@
 package net.codejava.supercsv;
 
+import javax.swing.*;
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class MainFunction
-{
+public class MainFunction {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
 
-        //Reading the CSV and putting it into a Map
-        String filename =  "src/main/java/Libraries.csv";
+        //Reading the CSV and putting it into a Map also creating a list of choices to use with the JOptionPane
+        String filename = "Playing_With_data/src/main/java/Libraries.csv";
         Map<String, Library> map = new HashMap<>();
-        new  ReadCSV(filename, map);
+        String[] choices = new String[83];
+        new ReadCSV(filename, map, choices);
 
 
-        System.out.println(map.get("Altgeld").toString());
+        //USER INTERFACE using JOptionPane
+        String input = (String) JOptionPane.showInputDialog(null, "Please select a library from the dropdown",
+                "Search Library Information", JOptionPane.QUESTION_MESSAGE, null,
+                choices, // Array of choices
+                choices[0]); // Initial choice
 
+        if (input.equals("--Select a Library--")) {
+            System.out.println("Please re run and select a Library");
+            JOptionPane.showMessageDialog(null, "Please re run and select a Library", "ERROR", JOptionPane.ERROR_MESSAGE, null);
 
+        } else {
+            System.out.println("Showing results for " + input);
+            System.out.println(map.get(input).toString());
+            JOptionPane.showMessageDialog(null, map.get(input).toString(), input + " Library Information:", JOptionPane.INFORMATION_MESSAGE, null);
+
+        }
 
     }
 
