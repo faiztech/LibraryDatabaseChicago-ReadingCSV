83c8e6faba60ef008ada865f82d9b458b3391dc8
faiztech <mfaizudd@neiu.edu>
2017-07-25 00:33:01 -0500

    Created test Directory

---
+++ Playing_With_data/src/test/net/codejava/supercsv/LibraryTest.java
@@ -0,0 +1,78 @@
+package net.codejava.supercsv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest
+{
+
+
+    @Test
+    void getName()
+    {
+    }
+
+    @Test
+    void setName()
+    {
+    }
+
+    @Test
+    void getHoursOfOp()
+    {
+    }
+
+    @Test
+    void setHoursOfOp()
+    {
+    }
+
+    @Test
+    void getAddress()
+    {
+    }
+
+    @Test
+    void setAddress()
+    {
+    }
+
+    @Test
+    void getWebsite()
+    {
+    }
+
+    @Test
+    void setWebsite()
+    {
+    }
+
+    @Test
+    void getPhone()
+    {
+    }
+
+    @Test
+    void setPhone()
+    {
+    }
+
+    @Test
+    void getZip()
+    {
+    }
+
+    @Test
+    void setZip()
+    {
+    }
+
+    @Test
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+
+}
---
+++ Playing_With_data/src/test/net/codejava/supercsv/ReadCSVTest.java
@@ -0,0 +1,9 @@
+package net.codejava.supercsv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ReadCSVTest
+{
+
+}
